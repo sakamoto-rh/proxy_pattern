4095ef73368bd249e13dab3d196ec5c2ae3dd1e9
稲川新太郎 <s-inagawa@inagawashintarou-no-MacBook-Air.local>
2013-11-02 15:23:44 +0900

    modify leader

--- lib/leader.rb
+++ lib/leader.rb
@@ -5,44 +5,76 @@ require './lib/base'
 
 class Leader
   include Base
+  attr_accessor :tired_point
+  #Dir.glob("./lib/*.rb"){|file| require file}
+
+  HEALTHY = 1
+  TIRED = 2
+  REST = 3
+  HEALTHY_MESSAGE = "元気はつらつオロナミンC"
+  TIRED_MESSAGE = "疲れました。シンドイDESU・・・"
+  REST_MESSAGE = "今日はやすみます。探さないでください。。。"
+
+  def initialize()
+    tired_point = rand(HEALTHY..REST)
+  end
   #
   # = 製品を開発します。リーダーなので能力が高いです。
   #
   def execute
+    tweet
+    return unless tired_point == REST
+
     product = Product.instance
-    product.progress += 2
-    product.quality += 2
 
-    s
+    if product.progress > 100
+      product.progress += 2
+      product.quality += 2
+    else
+      p "やっとおわりました！！(^^ゞ"
+    end
+  end
 
+  # メンバーの状況をヒアリング
+  def houkoku(product)
+    tweet
+    return unless tired_point == REST
   end
 
   private
-  #
-  # == プログラマーの連絡先を知っています
-  #
-  def project_manager
-    @project_manager || = ProjectManager.new
+  #  リーダのつぶやき
+  def tweet
+    case tired_point
+    when HEALTHY
+      p HEALTHY_MESSAGE
+    when TIRED
+      p TIRED_MESSAGE
+    else REST
+      p REST_MESSAGE
+    end
   end
+  ##
+  ## プロジェクトマネージャー の連絡先を知っています
+  ##
+  #def project_manager
+  #  @project_manager || = ProjectManager.new
+  #end
 
-  #
-  # == プログラマーの連絡先を知っています
-  #
-  def programmer
-    @programmer || = Programmer.new
-  end
+  ##
+  ## == プログラマーの連絡先を知っています
+  ##
+  #def programmer
+  #  @programmer || = Programmer.new
+  #end
 
-  #
-  # == リーダーの連絡先を知っています
-  #
-  def leader
-    @leader || = Leader.new
-  end
+  ##
+  ## == テスターの連絡先を知っています
+  ##
+  #def tester
+  #  @tester || = Tester.new
+  #end
 
-  #
-  # == 営業の連絡先を知っています
-  #
-  def sales
-    @sales || = Sales.new
-  end
 end
+
+leader = Leader.new
+leader.execute
