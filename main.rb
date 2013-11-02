884912d25539bf1604db6147eae85afe9e951480
ohta <ohta@raw-hide.co.jp>
2013-11-02 14:08:02 +0900

    bug fix

--- main.rb
+++ main.rb
@@ -1,7 +1,15 @@
 #
 # = メイン処理クラス
 #
+Dir.glob("./lib/*.rb"){|file| require file}
+Dir.glob("./schenarios/*.rb"){|file| require file}
+
 class Main
   def execute
+    product = Product.instance
+    Programmer.new.execute product
   end
 end
+
+
+Main.new.execute
