96bac8fa25242b8745cd5a651a678dbefbb14a46
坂本 <sakamoto@MacBook-Air.local>
2013-11-02 16:12:16 +0900

    一旦コミット

--- scenarios/proto_typing.rb
+++ scenarios/proto_typing.rb
@@ -22,26 +22,31 @@ module Scenarios
       wait 2
 
       #productの進捗が25になるまで繰り返す。
+      next_check = 10
       while product.progress < 25
 
         #開発する
         members[:project_customer].execute
-     
+
+        if product_progress > next_check
+        next_check += 10
+
         #顧客は4分の3の確率でパニックに陥る。
         puts "顧客が様子を見に来た..."
         wait 2
 
         random = rand(3)
-        puts ["顧客は満足そうだ。", "顧客の理解の範疇を超えた"]
+        puts ["顧客は満足そうだ。", "顧客の理解の範疇を超えた!!", "顧客が思っていたモノと違っている!!"][random]
         if random > 0
-          puts ["顧客はを理解していない!!", ""]
+         members[:customer].panic!
         end
 
       end
 
 
       #最終的にもう一回見せる。
-      members[:project_manager].
+      members[:project_manager]
     end
   end
 end
+
