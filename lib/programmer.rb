e5ecdff4cdcf137e1605c20d5646b0e8b3c3c824
michigoe <michigoe@efourtion.jp>
2013-11-02 15:24:23 +0900

    プログラマー、テスターの実装

--- lib/programmer.rb
+++ lib/programmer.rb
@@ -6,7 +6,38 @@ class Programmer
   #
   # == 製品を開発します
   #
+  attr_accessor :tired_point_programmer #テスター疲労度
+  def initialize
+    @tired_point_programmer = 1
+  end
+
   def execute(product)
+    @tired_point_programmer += 1
     product.progress += 1
+    if @tired_point_programmer == 0
+      p "【プログラマーが発言している】"
+      p "すいません、休んだ分取り返します！"
+    elsif @tired_point_programmer < 5
+      p "【プログラマーが発言している。】"
+      p "まだまだ楽勝です！"
+    elsif < 10
+      p "【プログラマーが発言している】"
+      p "ちょっときついです。。。"
+    elsif < 15
+      p "【プログラマーの様子が。。。】"
+      p "う、お腹が痛い。。。"
+    elsif < 20
+      p "【プログラマーからメールがあった。】"
+      p "体調不良で今日休みます。。。"
+      @tired_point_programmer = 0
+      product.progress -= 1
+    end
+  end
+ 
+  def call_from_customer
+    p "ごめんなさい直します。。。"
+    product.quality -= 3
+    product.progress -= 1
+    @tired_point_proggramer += 3
   end
 end
--- lib/tester.rb
+++ lib/tester.rb
@@ -5,7 +5,31 @@ class Tester
   #
   # == 製品をテストします
   #
+  attr_accessor :tired_point_test #テスター疲労度
+  def initialize
+    @tired_point_test = 1
+  end
+
   def execute(product)
     product.quality += 1
+    @tired_point_test += 2
+
+    if @tired_point_test == 0
+      p "今日からまた頑張ります！！！"
+    elsif @tired_point_test < 5
+      p "【テスターが発言している。】"
+      p "まだまだ楽勝です！"
+    elsif < 10
+      p "【テスターが文句をいっている】"
+      p "バグばっかりじゃねーかよ。。。"
+    elsif < 15
+      p "【テスターの様子がおかしい。】"
+      p "最近、夢にテストシナリオが出てくる。。。"
+    elsif < 20
+      p "【テスターから連絡があった。】"
+      p "体調不良で今日休みます。。。"
+      @tired_point_test = 0
+      product.quality -= 1
+    end
   end
 end
